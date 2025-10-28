f39c53ba5d0bdfe0916a5a664005e2b1311f7944
Johanna Wirell <jw223gx@student.lnu.se>
2025-10-28 18:03:19 +0100

    Update header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,12 +9,16 @@ export const Header = () => {
     <>
       <div className="container-header">
         <div className="profile">
-          <h3>Hej, Jag heter</h3>
+          <h3>Hej! Jag heter</h3>
           <h1>Johanna Wirell</h1>
           <h3>Fullstack developer</h3>
-          <h4>Jag arbetar idag som mjukvaruutvecklare och teamledare på Saab AB, där jag utvecklar
-            modeller och mjukvara för flygplanssimulering. Jag trivs i rollen som problemlösare och 
-            brinner för teknik, struktur och kreativ utveckling.</h4>
+          <h4>Jag arbetar idag som mjukvaruutvecklare och teamledare på Saab AB, där jag utvecklar avancerade 
+            modeller och mjukvara för flygplanssimulering. I mitt arbete kombinerar jag teknisk precision med 
+            ett strukturerat och analytiskt arbetssätt för att skapa pålitliga och innovativa lösningar.
+          </h4>
+          <h4>Jag trivs i rollen som problemlösare och brinner för att förena teknik, struktur 
+            och kreativ utveckling – både i kod och i samarbetet med kollegor.
+          </h4>
         </div>
         <div className="image">
           <img src={ME_PATH} alt="Couldn't find asset" />
